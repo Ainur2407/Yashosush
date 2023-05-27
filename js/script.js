45a5520ab9ff05508a953f2c8a0bfe3d64d18722
Ainur2407 <134147209+Ainur2407@users.noreply.github.com>
2023-05-27 15:33:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("active");
+    
+}
+
+
+
+
+
